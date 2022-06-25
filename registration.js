5fef729ef773b0d48235190ec88d86ed4f198c6f
Euglen Mulaj <euglen.mulaj@dunegroup.it>
2022-06-25 20:46:43 +0200

    first commit

---
+++ registration.js
@@ -0,0 +1,25 @@
+//
+$("#contact-form").on("submit", function (event) {
+    event.preventDefault();
+    $.LoadingOverlay("show");
+  
+    let formDataArray = $("#contact-form")
+      .serializeArray()
+      .reduce(function (obj, item) {
+        obj[item.name] = item.value;
+        return obj;
+      }, {});
+    console.log(formDataArray);
+    $.post("/api/register", formDataArray)
+      .done(function (response) {
+        console.log(response);
+        $("#contact-form").trigger("reset");
+        toastr.success("Il tuo account manager ti contatterà nelle prossime 24 ore per darti il ​​benvenuto.");
+      })
+      .fail(function (jqxhr, settings, ex) {
+        console.log(ex);
+      })
+      .always(function () {
+        $.LoadingOverlay("hide");
+      });
+  });
