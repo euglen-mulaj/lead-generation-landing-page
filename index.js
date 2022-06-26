876198dbbe432ff0b9420c1d56acc8f069f426f2
Euglen Mulaj <euglen.mulaj@dunegroup.it>
2022-06-26 12:39:22 +0200

    fixed spinner

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const morgan = require("morgan");
 const { Prohairesis } = require("prohairesis");
 const bodyParser = require("body-parser");
 const { response } = require("express");
-require('dotenv').config();
-const validator = require('validator');
+require("dotenv").config();
+const validator = require("validator");
 const path = require("path");
 
 const app = express();
@@ -13,35 +13,33 @@ const port = process.env.PORT || 8888;
 
 //connection to local db - mysql
 const mySQLString =
-"mysql://" + process.env.DB_USERNAME + ":"+ process.env.DB_PASSWORD + "@" +process.env.DB_HOSTNAME+"/"+process.env.DB_DEFAULT_SCHEMA+"?reconnect=true";
+  "mysql://" +
+  process.env.DB_USERNAME +
+  ":" +
+  process.env.DB_PASSWORD +
+  "@" +
+  process.env.DB_HOSTNAME +
+  "/" +
+  process.env.DB_DEFAULT_SCHEMA +
+  "?reconnect=true";
 const database = new Prohairesis(mySQLString);
 
 app
 
-
   .use(morgan("dev"))
-  //   .use('/',express.static("public"))
-//   .use("/static", express.static(path.join(__dirname, "static")))
-  //parse application/x-www-form-urlencoded
   .use(bodyParser.urlencoded({ extended: false }))
   .use("/assets", express.static(path.join(__dirname, "assets")))
-
 
   //parse application/json
   .use(bodyParser.json())
 
   .get("/", (req, res) => {
- 
-      res.sendFile(__dirname +"/registration.html");
-
+    res.sendFile(__dirname + "/registration.html");
   })
 
-  // .get("/registration", function (request, response) {
-  
-  //     response.sendFile(__dirname + "/registration.html");
-    
-  // })
-
+  .get("/registration", function (request, response) {
+    response.sendFile(__dirname + "/registration.html");
+  })
 
   .post("/api/register", async (req, res) => {
     const body = req.body;
@@ -54,11 +52,15 @@ app
     console.log(mobile);
 
     //backend validation
-    if(!validator.isEmail(email) || validator.isEmpty(fullname) || validator.isEmpty(req.body.phone)){
-        //Throw an error
-        let jsonResponse = { status: "KO", message: "Invalid data" };
-        res.status(422).json(jsonResponse);
-        return;
+    if (
+      !validator.isEmail(email) ||
+      validator.isEmpty(fullname) ||
+      validator.isEmpty(req.body.phone)
+    ) {
+      //Throw an error
+      let jsonResponse = { status: "KO", message: "Invalid data" };
+      res.status(422).json(jsonResponse);
+      return;
     }
 
     await database.execute(
@@ -86,9 +88,4 @@ app
     res.status(200).json(jsonResponse);
   })
 
-
-
-
   .listen(port, () => console.log(`Server listening on port ${port}`));
-
-
